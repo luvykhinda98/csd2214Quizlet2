ed50e5cb4f04be20eaeb71fb1dd7572b19684038
Lovepreet Singh <luvykhinda98@gmail.com>
2019-06-26 15:03:07 -0400

    fds commit

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,4 +1,15 @@
 You, a few seconds ago | 1 WebAuthnAssertion(you)
-waiting(3000);
+    //waiting(3000);
 
-console.log("function call has just ended");
+//console.log("function call has just ended");
+function waitingFn(timeInMs) {
+    const futureTime = Date.now() + timeInMs;
+
+    while (futureTime > Date.now()) {
+        // waiting
+
+    }
+
+}
+debugger;
+waitingFn(3000);
